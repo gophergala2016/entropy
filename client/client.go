b70f29676662dfc4e730f463a79c606aae5c6ff7
Tanguy Herrmann <dolanorgit@evereska.org>
2016-01-24 04:01:15 +0100

    Adds a menu to display a list of opponents to choose from

--- client/client.go
+++ client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/gophergala2016/entropy/models"
 	"github.com/gophergala2016/entropy/net"
@@ -9,6 +10,8 @@ import (
 	"os"
 	"os/signal"
 	"os/user"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -22,8 +25,10 @@ func handleInterruptSignal(ws *websocket.Conn) {
 	log.Println("Program killed")
 	os.Exit(0)
 }
+
+var username string
+
 func main() {
-	var username string
 	if len(os.Args) > 1 {
 		username = os.Args[1]
 	} else if utmp, err := user.Current(); err == nil {
@@ -54,10 +59,46 @@ func main() {
 		websocket.JSON.Receive(ws, &msg)
 		switch {
 		case msg.ResponseUserList != nil:
-			for k, _ := range msg.ResponseUserList.GamePlayers {
-				fmt.Println("User: ", k)
-			}
+			displayUserList(&msg.ResponseUserList.GamePlayers)
 		}
 		time.Sleep(1 * time.Second)
 	}
 }
+
+func displayUserList(gp *models.GamePlayers) {
+	fmt.Println("Connected Users:")
+	i := 0
+	userlist := make([]string, 0, len(*gp))
+	for k, _ := range *gp {
+		if k == username {
+			continue
+		}
+		fmt.Printf("%1d) %s\n", i, k)
+		userlist = append(userlist, k)
+		i++
+	}
+	correctInput := false
+	for !correctInput {
+		fmt.Println("\nSelect your opponent (number ID): ")
+		reader := bufio.NewReader(os.Stdin)
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			log.Println(err)
+		}
+		userid, err := strconv.Atoi(strings.TrimSpace(line))
+		if err != nil {
+			fmt.Println("Not a correct number")
+		} else {
+			if len(userlist) <= userid {
+				fmt.Println("Bad number, or list empty")
+				correctInput = true
+				continue
+			}
+			fmt.Println("Chosen", userlist[userid], "from", userlist)
+			correctInput = true
+		}
+
+	}
+
+}
+
